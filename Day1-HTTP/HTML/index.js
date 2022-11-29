8e6666a930703cfea28dea9438659ac0943d453e
nico-gao <jtgao99@gmail.com>
2022-11-29 13:23:55 -0800

    Initial commit

---
+++ Day1-HTTP/HTML/index.js
@@ -0,0 +1,27 @@
+// HTTP request: GET | POST | PUT | PATCH | DELETE (CRUD)
+// GET: retreive data from server
+// POST: submit data to server, create new resource
+// PUT/PATCH: updating, put - update the entire data object, patch - update certain fields
+// DELETE: delete
+
+// Status code
+// 100 - informational responses
+// 200 - ok, successful
+// 300 - redirection
+// 400 - client error
+// 500 - server error
+
+// console.log("hello");
+
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+
+// ECMAScript, syntax standard, es6 2015
+// Javascript - ECMAScript + WebAPI
+// NodeJS - ECMAScript + NodeAPI
+
+var element = document.getElementById("root");
+console.log(element.id);
